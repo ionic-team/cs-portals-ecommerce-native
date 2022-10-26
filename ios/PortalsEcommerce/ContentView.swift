35391ce49aa6936bbd067591703c4738afc9a9de
Eric Horodyski <horodyski@ionic.io>
2022-10-26 14:29:28 -0400

    chore(ios): create iOS workspace

---
+++ ios/PortalsEcommerce/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PortalsEcommerce
+//
+//  Created by Eric Horodyski on 10/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios/PortalsEcommerce/PortalsEcommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortalsEcommerceApp.swift
+//  PortalsEcommerce
+//
+//  Created by Eric Horodyski on 10/26/22.
+//
+
+import SwiftUI
+
+@main
+struct PortalsEcommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
