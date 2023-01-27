85d3d131719eabccb492c92a6f3080b337d19bd4
Eric Horodyski <horodyski@ionic.io>
2023-01-27 13:44:21 -0500

    feat(android): setup API secret

--- android/app/src/main/java/io/ionic/cs/portals/ecommerce/MainActivity.java
+++ android/app/src/main/java/io/ionic/cs/portals/ecommerce/MainActivity.java
@@ -3,6 +3,7 @@ package io.ionic.cs.portals.ecommerce;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // context.getString outside of Activity
+        String apiKey = getString(R.string.portals_api_key);
+        Log.v("ERIC", apiKey);
     }
 }
